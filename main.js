764c25a1b4c9f8446ba24dbf6c3edf631b3a6a86
Mubaraklouis@gmail.com <Mubaraklouis@gmail.com>
2022-06-04 14:57:17 +0200

    get the tailwind start from the new pc

--- main.js
+++ main.js
@@ -11,6 +11,17 @@ function addToDom(event) {
   var name=document.querySelector('input[name=name]').value;
   const price=document.querySelector('input[name=price]').value
  ///add to dom
+  itemList=[];
+  //data
+  const data={
+    "name":name,
+    "price":price
+  }
+  //
+  //localStorage
+  itemList.push(data)
+localStorage.setItem('data',itemList)
+//grapping the parent dev
  
   const parentItems=document.querySelector('#items-parent');
  
@@ -24,30 +35,45 @@ function addToDom(event) {
     errors.push(error)
     //console.log(error)
     //Display the error througth the errors
-    document.querySelector
+    for(let i=0; i<errors.length; i++){
+    document.querySelector('#form').innerHTML=`<h6 class="error">${errors[i].ms}</h6>`
+    }
   }
 
 else if (name.length<5||name.length>10){
   error={
     "ms":"Enter a valid item"
   }
-  console.log()
+  errors.push(error)
+  //displaying error ms
+  for(let i=0; i<errors.length; i++){
+    document.querySelector('#form').innerHTML=`<h6 class='error'>${errors[i].ms}</h6>`
+    }
+
 }
   else{
+for(let i=0; i<itemList.length; i++){
+   
+ 
   var  Ul=document.createElement('ul')
   Ul.className="items";
-  const ElementUl=`
-    <li>${name}</li>
-     <li>${price}</li>
+  ///using var instead of const in order to access the variable outside the loop block
+  var ElementUl=`
+    <li>${localStorage.getItem('data[i].name')})</li>
+     <li>${localStorage.getItem('data[i].price')}</li>
       <button onclick="deletItem()" class="delete"></button>`
+}
       Ul.innerHTML=ElementUl;
-  parentItems.appendChild(Ul);
+  parentItems.apendChild(Ul);
+  //succes ms
   
-
+document.querySelector('#form').innerHTML=`<h6 class="success">Item added successfully</h6>`
   }
  //clear form data
+ /*
  document.querySelector('input[name=name]').value=''
  document.querySelector('input[name=price]').value='';
+ */
 }
  
 ///delete function
@@ -55,4 +81,53 @@ else if (name.length<5||name.length>10){
 function deletItem(){
   const item=document.querySelector('.items')
   item.remove()
+  //delete message
+  document.querySelector('#form').innerHTML=`<h6 class="success">Item deleted successfully</h6>`
+  
 }
+
+
+//const k=document.querySelector('.form-child');
+
+//k.addEventListener('submit',checkingList)
+  
+  /*
+function checkingList(){
+  
+   ////gapping the data from dom
+   //form btn
+   
+var Name=document.querySelector('input[name=name]').value;
+  const Price=document.querySelector('input[name=price]').value;
+  //schema for the form data
+  const FormData={
+    "name":Name,
+    "price":Price
+  }
+  //data base
+  var itemList_save=[];
+  
+ itemList_save.push(FormData)
+  
+ // console.log(itemList_save[0])
+ 
+
+ // const AvailableItems_save=itemList_save;
+  
+  for(let i=0; i<itemList_save.length; i++){
+    const name=itemList_save[i].name.length;
+    
+   if(name!=0){
+      console.log("items exist")
+    }
+    else if(name===0){
+      console.log("nothing")
+    }
+    console.log(name)
+  }
+  
+//console.log(itemList_save[1].name)
+ 
+  // AvailableItems.length=0;
+}
+*/
